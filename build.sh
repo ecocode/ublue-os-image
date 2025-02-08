b7a1c4208936ae28aca831e68f19d3e814da3fb5
erik colson <eco@ecocode.net>
2025-02-09 00:12:12 +0100

    zathura plugins added

--- build.sh
+++ build.sh
@@ -100,7 +100,7 @@ rpm-ostree install the_silver_searcher ripgrep
 # rpm-ostree install cuda-toolkit nvidia-gds
 
 # text pdf stuff
-rpm-ostree install odt2txt pandoc zathura typst
+rpm-ostree install odt2txt pandoc zathura zathura-pdf-poppler zathura-djvu zathura-ps typst
 
 # network tools
 rpm-ostree install netscanner nmap sshfs
