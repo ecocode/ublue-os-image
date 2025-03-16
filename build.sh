d9b96c36d17b1ac38f8100bd751b730fb85afdf1
erik colson <eco@ecocode.net>
2025-03-16 13:37:34 +0100

    added wpaperd, removed variety

--- build.sh
+++ build.sh
@@ -69,7 +69,7 @@ rpm-ostree install cosmic-desktop miracle-wm niri xwayland-satellite
 # removed: hyprland hyprland-devel cmake meson cpio
 rpm-ostree install strace
 # - sway
-rpm-ostree install systemd trash-cli gdu duc usbip udisks2 udiskie variety virt-install virt-manager virt-top virt-viewer w3m
+rpm-ostree install systemd trash-cli gdu duc usbip udisks2 udiskie wpaperd virt-install virt-manager virt-top virt-viewer w3m
 # - wayfire
 rpm-ostree install tlp powertop
 # rpm-ostree install power-profiles-daemon
