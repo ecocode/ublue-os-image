65a640f871d44457513a39b5d15ff3d0fff14919
erik colson <eco@ecocode.net>
2025-05-30 10:59:23 +0200

    added drm_info for nvidia info

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ rpm-ostree install eza fastfetch fbreader fd-find firewall-config foot foot-term
 # - insync
 # - intel-media-driver
 rpm-ostree install j4-dmenu-desktop jrnl kalendar kamera kf5-solid kf6-solid kio-gdrive kio-extras-kf5 kio-fuse kio-ftps kjournald kontact ksystemlog kwallet-pam kanshi libimobiledevice libimobiledevice-utils libusb light lm_sensors lshw luarocks lynx macchanger mako mediainfo mpv neochat network-manager-applet NetworkManager-tui catimg
-rpm-ostree install inxi nvtop
+rpm-ostree install inxi nvtop drm_info
 # rpm-ostree install youtube-dl
 # - niri
 # - nnn
