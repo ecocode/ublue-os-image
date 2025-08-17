d297650476506689d3539cbc8e64821143a6d2df
erik colson <eco@ecocode.net>
2025-08-17 16:28:40 +0200

    libseat was wrong

--- build.sh
+++ build.sh
@@ -91,7 +91,7 @@ rpm-ostree install slurp wdisplays wl-clipboard wofi xdg-desktop-portal-wlr sway
 rpm-ostree install river rofi-wayland bemenu
 rpm-ostree install ibus gnome-keyring
 # for compiling wlroots myself for mangowc
-rpm-ostree install egl-wayland libglvnd-egl libseat wayland-protocols wayland-devel mesa-libEGL-devel mesa-libGLES-devel mesa-dri-drivers xorg-x11-server-Xwayland libdrm-devel libgbm-devel libxkbcommon-devel libudev-devel pixman-devel libinput-devel libevdev-devel systemd-devel cairo-devel libpcap-devel json-c-devel pam-devel pango-devel pcre-devel gdk-pixbuf2-devel hwdata-devel
+rpm-ostree install egl-wayland libglvnd-egl libseat-devel wayland-protocols-devel wayland-devel mesa-libEGL-devel mesa-libGLES-devel mesa-dri-drivers xorg-x11-server-Xwayland libdrm-devel libgbm-devel libxkbcommon-devel libudev-devel pixman-devel libinput-devel libevdev-devel systemd-devel cairo-devel libpcap-devel json-c-devel pam-devel pango-devel pcre-devel gdk-pixbuf2-devel hwdata-devel
 
 # system tools
 rpm-ostree install borgbackup vorta
