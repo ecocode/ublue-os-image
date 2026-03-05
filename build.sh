b4fbb95425ec2b8ac2cfd6655474dae54c1c1384
erik colson <eco@ecocode.net>
2026-03-05 18:34:45 +0100

    added podman-compose

--- build.sh
+++ build.sh
@@ -113,7 +113,7 @@ rpm-ostree install -C kde-wallpapers plasma-workspace-wallpapers arc-kde-wallpap
 # removed: hyprland hyprland-devel cmake meson cpio
 rpm-ostree install -C strace
 # - sway
-rpm-ostree install -C systemd trash-cli gdu duc usbip fwupd udisks2 udiskie wev wpaperd virt-install virt-manager virt-top virt-viewer virtio-win edk2-ovmf swtpm swtpm-tools cockpit cockpit-machines cockpit-ostree cockpit-podman w3m samba ddclient
+rpm-ostree install -C systemd trash-cli gdu duc usbip fwupd udisks2 udiskie wev wpaperd virt-install virt-manager virt-top virt-viewer virtio-win edk2-ovmf swtpm swtpm-tools cockpit cockpit-machines cockpit-ostree cockpit-podman podman-compose w3m samba ddclient
 # - wayfire
 rpm-ostree install -C tlp powertop acpi
 # rpm-ostree install power-profiles-daemon
