ea984c9abd289b43f7005a6f72764d8d27446125
erik colson <eco@ecocode.net>
2026-03-10 01:16:45 +0100

    corrections

--- Containerfile
+++ Containerfile
@@ -40,9 +40,9 @@ ARG SOURCE_IMAGE="aurora"
 # - stable-zfs
 # - stable-nvidia-zfs
 # - (and the above with testing rather than stable)
-# ARG SOURCE_SUFFIX="-nvidia-open"
+ARG SOURCE_SUFFIX="-nvidia-open"
 # EC: for kinoite and bazzite
-ARG SOURCE_SUFFIX="-nvidia"
+# ARG SOURCE_SUFFIX="-nvidia"
 
 ## SOURCE_TAG arg must be a version built for the specific image: eg, 39, 40, gts, latest
 ARG SOURCE_TAG="latest"
--- build.sh
+++ build.sh
@@ -184,7 +184,7 @@ rpm-ostree install -C gimp3 dia flameshot
 # rpm-ostree install /custom-rpm/ARES-Commander-2026-26.3.1.4145-x86_64.rpm
 # rpm-ostree install /custom-rpm/expressvpn-3.69.0.0-1.x86_64.rpm
 
-rpm-ostree install https://www.graebert.com/cad-software/download/ares-commander/linux/rpm/latest
+rpm-ostree install https://files.graebert.com/ares/26.3.1.4145/ARES-Commander-2026-26.3.1.4145-x86_64.rpm
 
 
 #### Example for enabling a System Unit File
