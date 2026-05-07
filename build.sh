02d31162434a6fe9e09c089473140f07e551b0e8
erik colson <eco@ecocode.net>
2026-05-07 14:24:47 +0200

    EMS stuff

--- build.sh
+++ build.sh
@@ -152,6 +152,10 @@ rpm-ostree install -C emacs global
 rpm-ostree install -C slurp wdisplays wl-clipboard wofi xdg-desktop-portal-wlr swaybg swayr swayidle swaylock sway-systemd waybar wf-recorder
 rpm-ostree install -C rofi-wayland bemenu walker elephant
 rpm-ostree install -C ibus gnome-keyring
+
+# for ewm
+rpm-ostree install -C systemd-devel
+
 # for compiling wlroots myself for mangowc - conflicting
 # rpm-ostree install egl-wayland libglvnd-egl libseat-devel wayland-protocols-devel wayland-devel mesa-libEGL-devel mesa-libGLES-devel mesa-dri-drivers xorg-x11-server-Xwayland libgbm-devel libxkbcommon-devel libudev-devel pixman-devel libinput-devel libevdev-devel systemd-devel cairo-devel libpcap-devel json-c-devel pam-devel pango-devel pcre-devel gdk-pixbuf2-devel hwdata-devel
 # rpm-ostree install libdrm libdrm-devel libdisplay-info
